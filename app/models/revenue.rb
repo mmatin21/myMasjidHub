ba4f8677857741d71764833563219db091fab13b
mmatin21 <makmatin17@gmail.com>
2024-09-06 19:28:16 -0400

    typo fix

--- app/models/revenue.rb
+++ app/models/revenue.rb
@@ -2,7 +2,7 @@ class Revenue < ApplicationRecord
   belongs_to :masjid
 
   validates :name, presence: true
-  validates :amount, prescence: true, numericality: { greater_than_or_equal_to: 0}
-  validates :revenue_date, prescence: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0}
+  validates :revenue_date, presence: true
 
 end
