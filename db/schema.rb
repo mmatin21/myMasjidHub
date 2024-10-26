427cb79f5059f61dd02081cffc0528aaed22a3bd
mmatin21 <makmatin17@gmail.com>
2024-10-26 04:23:55 -0400

    fixing db stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_26_074543) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_12_211141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -107,6 +107,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_26_074543) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "email"
     t.index ["reset_password_token"], name: "index_masjids_on_reset_password_token", unique: true
   end
 
