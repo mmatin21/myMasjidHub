a5828503ee3c2f4045ae7bedcfba45a10a785488
mmatin21 <makmatin17@gmail.com>
2024-10-26 15:03:21 -0400

    more test stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV['RENDER'].present?
   config.action_cable.allowed_request_origins = [/https:\/\/*/]
 
   # Compress CSS using a preprocessor.
