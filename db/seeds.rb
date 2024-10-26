0046f9d8a51376eb052ddc213bdb9d782626b368
mmatin21 <makmatin17@gmail.com>
2024-10-26 04:53:39 -0400

    seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+test_masjid = Masjid.create(address: "Test Address", city: "Test City", state: "Test State",
+ zipcode: "Test Zip", phone_number: "1234567890", email: "test@test.com", password: "test", password_confirmation: "test")
