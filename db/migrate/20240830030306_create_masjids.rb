875a622102e5e91f488e5456d92c321e83c0a108
mmatin21 <makmatin17@gmail.com>
2024-10-26 04:04:28 -0400

    staging test env

--- db/migrate/20240830030306_create_masjids.rb
+++ db/migrate/20240830030306_create_masjids.rb
@@ -1,11 +1,11 @@
 class CreateMasjids < ActiveRecord::Migration[7.0]
   def change
     create_table :masjids do |t|
+      t.string :name
       t.string :address
       t.string :city
       t.string :state
       t.string :zipcode
-      t.string :email
       t.string :phone_number
       
       t.timestamps
