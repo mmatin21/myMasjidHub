21c326f206a9844106897d4b513bb83257e97da5
mmatin21 <makmatin17@gmail.com>
2024-10-26 16:02:59 -0400

    revert some of the old db stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV['RENDER'].present?
-  config.action_cable.allowed_request_origins = [/https:\/\/*/]
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
