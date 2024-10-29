458d36d321f9829d385a387b4092e41ef5d9de88
mmatin21 <makmatin17@gmail.com>
2024-10-28 22:23:07 -0400

    add donations to donation tabke

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -4,7 +4,6 @@ class Donation < ApplicationRecord
   belongs_to :masjid
 
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0}
-  validates :currency, presence: true
 
   def self.ransackable_attributes(auth_object = nil) 
     ["created_at", "amount"]
--- db/migrate/20240831015206_create_donations.rb
+++ db/migrate/20240831015206_create_donations.rb
@@ -1,9 +1,9 @@
 class CreateDonations < ActiveRecord::Migration[7.0]
   def change
     create_table :donations do |t|
-      t.references :attendee, null: false, foreign_key: true
+      t.references :attendee, foreign_key: true
       t.references :fundraiser, null: false, foreign_key: true
-      t.references :masjid, null: false, foreign_key: true
+      t.references :masjid, foreign_key: true
       t.decimal :amount, precision: 10, scale: 2
       t.string :currency
 
