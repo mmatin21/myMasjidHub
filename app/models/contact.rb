7e8b394b0cf247db7dcf5b53764b2e9346f81b0c
mmatin21 <makmatin17@gmail.com>
2024-12-25 18:53:54 -0500

    contact and reveneus show and edit

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,6 +7,10 @@ class Contact < ApplicationRecord
     "#{first_name} #{last_name} #{middle_name} (#{email})"
   end
 
+  def name
+    "#{first_name} #{middle_name} #{last_name}"
+  end
+
   def self.ransackable_attributes(auth_object = nil) 
     ["first_name", "last_name", "email", "phone_number"]
   end
