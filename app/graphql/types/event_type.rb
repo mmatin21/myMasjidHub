a2b0ddb18288544246f32b8b4a9fe75cd3976747
mmatin21 <makmatin17@gmail.com>
2024-12-30 20:28:10 -0500

    formatting dates

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -13,5 +13,10 @@ module Types
     field :zipcode, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :formatted_event_date, String, null: false
+
+    def formatted_event_date
+      object.event_date.strftime("%A, %B %e") # Example: "2000-01-01 13:30"
+    end
   end
 end
--- app/graphql/types/prayer_type.rb
+++ app/graphql/types/prayer_type.rb
@@ -9,5 +9,16 @@ module Types
     field :iqaamah, GraphQL::Types::ISO8601DateTime
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :formatted_adhaan, String, null: false
+    field :formatted_iqaamah, String, null: false
+
+    def formatted_adhaan
+      object.adhaan.strftime("%l:%M %p") # Example: "2000-01-01 13:30"
+    end
+
+    def formatted_iqaamah
+      object.iqaamah.strftime("%l:%M %p") # Example: "2000-01-01 13:30"
+    end
   end
 end
