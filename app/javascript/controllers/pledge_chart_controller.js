a0919f444168ab278e7c41468923de23fc32e471
mmatin21 <makmatin17@gmail.com>
2025-01-02 20:23:42 -0500

    dashboard styling

--- app/javascript/controllers/pledge_chart_controller.js
+++ app/javascript/controllers/pledge_chart_controller.js
@@ -102,7 +102,7 @@ export default class extends Controller {
       },
       grid: {
         show: true,
-        
+        strokeDashArray: 4,
         padding: {
           left: 10,
           right: 2,
