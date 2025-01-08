83726dd6d31cad96c37946aef64e2ff4a759f93a
mmatin21 <makmatin17@gmail.com>
2025-01-07 23:00:37 -0500

    some show page changes and getting rid of unneeded routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,30 +3,21 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
+
   resources :pledges
   resources :contacts
-  resources :balances
   resources :prayers
   resources :donations
   resources :fundraisers
-  devise_for :attendees
   resources :events
   resources :revenues
+  resources :expenses
+  resources :landing
+
   devise_for :masjids, path: 'masjids', controllers: {
     sessions: "masjids/sessions",
     registrations: "masjids/registrations"
   }
-  resources :expenses
-  resources :months do
-    collection do
-      get :months
-    end
-  end
-  resources :options do
-    collection do
-      get :pledges
-    end
-  end
   resources :masjids do
     resources :payouts, only: [:new, :create]
     resources :dashboard, only: [:index]
@@ -35,8 +26,21 @@ Rails.application.routes.draw do
       get :open_stripe_dashboard
     end
   end
+
+  resources :months do
+    collection do
+      get :months
+    end
+  end
+
+  resources :options do
+    collection do
+      get :pledges
+    end
+  end
+
   post '/webhooks/stripe', to: 'webhooks#stripe'
-  resources :landing
+
   resources :bulk_deletes, only: [] do
     collection do
       delete :expenses_and_revenues
