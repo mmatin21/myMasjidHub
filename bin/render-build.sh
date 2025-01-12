43fb378e147270c10d480b3c0a8d73caaa43a592
mmatin21 <makmatin17@gmail.com>
2025-01-12 00:12:58 -0500

    rails qa javascript work around attempt 2

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,9 @@
 set -o errexit
 
 bundle install
+bundle exec rails assets:clobber
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails assets:clobber
 
 
 # If you're using a Free instance type, you need to
