73790a5fcf536ca0b903a23e544b077c1d3ff631
mmatin21 <makmatin17@gmail.com>
2025-01-29 18:56:26 -0500

    fix for donation form

--- app/javascript/controllers/search_select_controller.js
+++ app/javascript/controllers/search_select_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus";
 import TomSelect from "tom-select";
 
 export default class extends Controller {
+
+  static targets = ["contact", "pledge"]
   
 
   connect() {
@@ -56,52 +58,38 @@ export default class extends Controller {
 
   handleCreate(input, callback) {
     // Open the modal and pass the input value
-    this.openModal(input);
-
+    if(this.hasContactTarget){
+      this.openContactModal(input);
+    }
+    else {
+      this.openPledgeModal(input);
+    }
     // Prevent Tom Select from adding the item immediately
     callback(null);
   }
 
-  openModal(value) {
+  openContactModal(value){
     const contactModal = document.getElementById("new-contact-modal");
+    const form = document.getElementById("parent-form");
+
+    contactModal.classList.remove("hidden");
+    form.classList.add("hidden")
+  }
+
+  openPledgeModal(value){
     const pledgeModal = document.getElementById("new-pledge-modal");
     const form = document.getElementById("parent-form");
 
     const contactId = document.getElementById("donation_contact_id")?.value;
     const fundraiserId = document.getElementById("donation_fundraiser_id")?.value;
 
-    const firstNameInput = document.getElementById("modal-first-name");
-    const lastNameInput = document.getElementById("modal-last-name");
-
     const pledgeContact = document.getElementById("pledge_contact_id");
     const pledgeFundraiser = document.getElementById("pledge_fundraiser_id");
-
-    // Split the value into first and last name
-    const [firstName, ...lastNameParts] = value.split(" ");
-    const lastName = lastNameParts.join(" ");
-
-    // Prefill the modal inputs
-    if (firstNameInput) firstNameInput.value = firstName || "";
-    if (lastNameInput) lastNameInput.value = lastName || "";
 
     if (pledgeContact) pledgeContact.value = contactId || "";
     if (pledgeFundraiser) pledgeFundraiser.value = fundraiserId || "";
 
-    // Show the modal
-    if (contactModal && contactId == "") {
-      contactModal.classList.remove("hidden");
-      form.classList.add("hidden")
-    }
-    else {
-      if(pledgeModal != null) {
-        pledgeModal.classList.remove("hidden");
-        form.classList.add("hidden");
-      }
-      else {
-        contactModal.classList.remove("hidden");
-        form.classList.add("hidden")
-      }
-      
-    }
+    pledgeModal.classList.remove("hidden");
+    form.classList.add("hidden");
   }
 }
