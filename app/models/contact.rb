ad7b6b3ab84b159dbec61566000719a927b4087d
mmatin21 <makmatin17@gmail.com>
2025-01-30 18:30:17 -0500

    polishing up some modal forms

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,13 @@ class Contact < ApplicationRecord
   has_many :donations
   has_many :pledges
 
+  before_validation :check_duplicate_email
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
+
   def full_name
     "#{first_name} #{last_name} #{middle_name} (#{email})"
   end
@@ -11,15 +18,16 @@ class Contact < ApplicationRecord
     "#{first_name} #{middle_name} #{last_name}"
   end
 
-  def self.ransackable_attributes(auth_object = nil) 
-    ["first_name", "last_name", "email", "phone_number"]
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[first_name last_name email phone_number]
   end
 
   def self.to_csv
     CSV.generate(headers: true) do |csv|
-      csv << ["first_name", "last_name", "email", "phone_number", "amount_pledged", "amount_donated"]
+      csv << %w[first_name last_name email phone_number amount_pledged amount_donated]
       all.each do |contact|
-        csv << [contact.first_name, contact.last_name, contact.email, contact.phone_number, "$#{contact.pledges.sum(:amount)}", "$#{contact.donations.sum(:amount)}"]
+        csv << [contact.first_name, contact.last_name, contact.email, contact.phone_number,
+                "$#{contact.pledges.sum(:amount)}", "$#{contact.donations.sum(:amount)}"]
       end
     end
   end
@@ -27,11 +35,19 @@ class Contact < ApplicationRecord
   def self.import(file, masjid_id)
     CSV.foreach(file.path, headers: true) do |row|
       contact_data = row.to_hash
-      contact_data["masjid_id"] = masjid_id # Attach the masjid_id
+      contact_data['masjid_id'] = masjid_id # Attach the masjid_id
 
       # Find existing expense or initialize a new one
-      contact = find_or_initialize_by(id: contact_data["id"]) # Use a unique identifier
+      contact = find_or_initialize_by(id: contact_data['id']) # Use a unique identifier
       contact.update(contact_data)
     end
+  end
+
+  private
+
+  def check_duplicate_email
+    return unless Contact.exists?(masjid_id: masjid_id, email: email)
+
+    errors.add(:email, 'is already associated with a contact in this masjid.')
   end
 end
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -6,21 +6,23 @@ class Donation < ApplicationRecord
   belongs_to :contact
   belongs_to :pledge, optional: true
 
-  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0}
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :contact, presence: true
 
-  def self.ransackable_attributes(auth_object = nil) 
-    ["created_at", "amount"]
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at amount]
   end
 
-  def self.ransackable_associations(auth_object = nil)
-    ["contact"]
+  def self.ransackable_associations(_auth_object = nil)
+    ['contact']
   end
 
   def self.to_csv
     CSV.generate(headers: true) do |csv|
-      csv << ["first_name", "last_name", "email", "fundraiser", "amount", "created_at"]
+      csv << %w[first_name last_name email fundraiser amount created_at]
       all.each do |donation|
-        csv << [donation.contact.first_name, donation.contact.last_name, donation.contact.email, donation.fundraiser.name, "$#{donation.amount}", donation.created_at.strftime("%m/%d/%Y")]
+        csv << [donation.contact.first_name, donation.contact.last_name, donation.contact.email,
+                donation.fundraiser.name, "$#{donation.amount}", donation.created_at.strftime('%m/%d/%Y')]
       end
     end
   end
@@ -28,25 +30,25 @@ class Donation < ApplicationRecord
   def self.import(file, masjid_id)
     CSV.foreach(file.path, headers: true) do |row|
       donation_data = row.to_hash
-      donation_data["masjid_id"] = masjid_id # Attach the masjid_id
+      donation_data['masjid_id'] = masjid_id # Attach the masjid_id
 
       # Find or initialize fundraiser by name and masjid_id
       fundraiser = Fundraiser.find_or_initialize_by(
-        name: donation_data["fundraiser"],
+        name: donation_data['fundraiser'],
         masjid_id: masjid_id
       )
       fundraiser.save if fundraiser.new_record?
 
       # Find or initialize contact by email and masjid_id
       contact = Contact.find_or_initialize_by(
-        email: donation_data["email"],
+        email: donation_data['email'],
         masjid_id: masjid_id
       )
       contact.save if contact.new_record?
 
       # Clean donation data to only include valid attributes
       donation_attributes = {
-        amount: donation_data["amount"].gsub(/[$,]/, '').to_f,
+        amount: donation_data['amount'].gsub(/[$,]/, '').to_f,
         fundraiser_id: fundraiser.id,
         contact_id: contact.id,
         masjid_id: masjid_id
@@ -55,7 +57,7 @@ class Donation < ApplicationRecord
       Rails.logger.info "Donation data: #{donation_attributes}"
 
       # Find existing donation or initialize a new one
-      donation = find_or_initialize_by(id: donation_data["id"])
+      donation = find_or_initialize_by(id: donation_data['id'])
       donation.update(donation_attributes)
       Rails.logger.info "Donation updated: #{donation.errors.full_messages}"
     end
