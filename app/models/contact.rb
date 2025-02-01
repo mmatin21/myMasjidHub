f5fdddf0a3335ca1b83a2ef760f481def6d5c13f
mmatin21 <makmatin17@gmail.com>
2025-01-31 20:15:27 -0500

    unrequire phone number for contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,6 @@ class Contact < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :phone_number, presence: true
 
   def full_name
     "#{first_name} #{last_name} #{middle_name} (#{email})"
