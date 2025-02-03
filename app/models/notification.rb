42fa571652cfaed1e6991d474d956b17babe2109
mmatin21 <makmatin17@gmail.com>
2025-02-03 02:57:11 -0500

    notification badge live updates

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,17 +4,23 @@ class Notification < ApplicationRecord
 
   # After a new notification is created, broadcast updates to the relevant targets
   after_create_commit do
-    # broadcast_replace_to "topbar_notifications_#{masjid.id}", target: 'notification-badge', partial: 'navbar/topbar/notification_badge', locals: {
-    #   masjid: masjid
-    # }
+    broadcast_update_to "bell_notifications_#{masjid.id}", target: 'notification-badge',
+                                                           partial: 'navbar/topbar/notification_badge',
+                                                           locals: {
+                                                             masjid: masjid
+                                                           }
 
-    broadcast_prepend_to "dropdown_#{masjid.id}", target: 'notification-dropdown-list', partial: 'navbar/topbar/notification_dropdown', locals: {
-      notification: self
-    }
+    broadcast_prepend_to "dropdown_#{masjid.id}", target: 'notification-dropdown-list',
+                                                  partial: 'navbar/topbar/notification_dropdown',
+                                                  locals: {
+                                                    notification: self
+                                                  }
 
-    broadcast_prepend_to "notifications_#{masjid.id}", target: 'notification-list', partial: 'notifications/notification_index', locals: {
-      notifications: self
-    }
+    broadcast_prepend_to "notifications_#{masjid.id}", target: 'notification-list',
+                                                       partial: 'notifications/notification_index',
+                                                       locals: {
+                                                         notifications: self
+                                                       }
   end
 
   scope :unread, -> { where(read_at: nil) }
