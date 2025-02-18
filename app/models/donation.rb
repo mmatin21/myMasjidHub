ccd22e54ff82d6a123c963c21d47ea46ed9e1729
mmatin21 <makmatin17@gmail.com>
2025-02-17 22:08:43 -0500

    year/month filtering on dashboard

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -10,6 +10,9 @@ class Donation < ApplicationRecord
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :contact, presence: true
 
+  scope :by_year, ->(year) { where('extract(year from created_at) = ?', year) }
+  scope :by_month, ->(month) { where('EXTRACT(MONTH FROM created_at) = ?', month) if month.present? }
+
   def self.ransackable_attributes(_auth_object = nil)
     %w[created_at amount]
   end
