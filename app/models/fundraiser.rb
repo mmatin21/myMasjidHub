83b132f68466d1eb498e0c87533ea2a3cbce9c3d
mmatin21 <makmatin17@gmail.com>
2025-02-26 02:42:20 -0500

    correct url for slugged fundraiser

--- app/models/fundraiser.rb
+++ app/models/fundraiser.rb
@@ -35,7 +35,7 @@ class Fundraiser < ApplicationRecord
   private
 
   def generate_qr_code
-    fundraiser_url = "https://mymasjidfinder.onrender.com//masjids/#{masjid_id}/fundraisers/#{id}"
+    fundraiser_url = "https://mymasjidfinder.onrender.com/masjids/#{masjid.slug}/fundraisers/#{slug}"
     qr = RQRCode::QRCode.new(fundraiser_url)
 
     qr_png = StringIO.new(qr.as_png(size: 300, border_modules: 2).to_s)
